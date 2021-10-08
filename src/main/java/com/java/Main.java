8fa4e1a0d34eb1dc7a128e5ae716a188cd9775c2
MSURASH <sufianrash22@gmail.com>
2021-10-07 20:07:53 -0400

    First commit

--- src/main/java/com/java/Main.java
+++ src/main/java/com/java/Main.java
@@ -3,7 +3,7 @@ package com.java;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("play with git develop...");
+		System.out.println("play with git develop a...");
 	}
 
 }
